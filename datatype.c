bd665cfcff7a087947194a9aa8fe11fcf9380b6c
liuke <1262946533@qq.com>
2020-06-08 17:53:24 +0800

    展示较为复杂的数据类型定义: 修改datatype.c

--- datatype.c
+++ datatype.c
@@ -5,9 +5,22 @@ char which;
 MPI_Datatype *newtp;
 {
 	int count = 2;
+	int stride = 25, length = 3;
+	int lengths[2] ={3, 2}, displs[2] = {0, 5};
+	
 	if (which == 'c')
 	{
 		MPI_Type_contiguous(count, MPI_INT, newtp);
+	}
+	if (which == 'v')
+	{
+		MPI_Type_vector(count, length, stride, MPI_INT, newtp);
+		
+	}
+	if (which == 'i')
+	{
+		MPI_Type_indexed(count, lengths, displs, MPI_INT, newtp);
+		
 	}
 	return;
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@ char **argv;
 {
 	MPI_Comm comm;
 	int np, iam;
-	int m, n, narray[100], marray[101];
+	int m, n, narray[100], marray[101], matr[10][25];
 	MPI_Datatype newtp;
 	MPI_Status st;
 	
@@ -20,19 +20,30 @@ char **argv;
 	{
 		narray[m] = m;
 	}
-	datatype('c', &newtp);
+	datatype('i', &newtp);
 	
 	MPI_Type_commit(&newtp);
 	if (iam == 2)
 	{
-		MPI_Send(narray + iam * 5, 3, newtp, 1, 5, comm);
-		//这样发送的是10处开始的数据，3个 newtp类型
+		// for (m = 0; m < 10; ++ m)
+		// {
+			// for (n = 0; n < 25; ++ n)
+			// {
+				// matr[m][n] = m + n;
+			// }
+		// }
+		// MPI_Send(matr, 1, newtp, 1, 5, comm);
+		MPI_Send(narray, 1, newtp, 1, 5, comm);
 	}
 	if (iam == 1)
 	{
-		MPI_Recv(marray, 3, newtp, 2, 5, comm, &st);
-		printf("\n Date on Proc %d  are %d, %d, %d, %d, %d, %d\n",\
-		iam, marray[0], marray[1], marray[2], marray[3], marray[4], marray[5]);
+		// MPI_Recv(matr, 1, newtp, 2, 5, comm, &st);
+		// printf("\n Date on Proc %d  are %d, %d, %d, %d, %d, %d\n",\
+		iam, matr[0][0], matr[0][1], matr[0][2], matr[1][0], matr[1][1], matr[1][2]);
+		
+		MPI_Recv(marray, 1, newtp, 2, 5, comm, &st);
+		printf("\n Date on Proc %d  are %d, %d, %d, %d, %d\n",\
+		iam, marray[0], marray[1], marray[2], marray[5], marray[6]);		
 	}
 	// printf("\nData type is created\n");
 	MPI_Type_free(&newtp);
